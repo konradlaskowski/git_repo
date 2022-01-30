1c373913ed7dbd7e5142d5c2083f563691be7a79
Konrad Laskowski <konrad374@gmail.com>
2022-01-30 17:55:42 +0100

    automated form working with selenium

--- seleniumTEST/main.py
+++ seleniumTEST/main.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 import random
+
 option = webdriver.ChromeOptions()
 option.add_argument("-incognito")
+#option.add_argument("--headless")
+#option.add_argument("disable-gpu")
 
 mails =["konrad374@gmail.com","grazyna123@gmail.com",'zenek234@gmail.com',"jacek456@gmail.com"]
 
 a = random.randint(0,3)
 buton_range=random.randint(0,2)
-browser=webdriver.chrome(executable_path="chromedriver.exe",options=option)
+browser = webdriver.Chrome(executable_path="chromedriver.exe",options=option)
 browser.get("https://docs.google.com/forms/d/e/1FAIpQLSfg-un5ZEzQe7y_EOZVeTrVGa1sCX-MjYDkfPqNvOLqTRNl6Q/viewform")
 
-textboxes=browser.find_element_by_class_name("quantumWizTextinputPaperinputInput")
+textboxes=browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")
 submit=browser.find_element_by_class_name("appsMaterialWizButtonPaperbuttonContent")
 
 
