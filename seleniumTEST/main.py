69d17e65e8faa1ddd53aba5fcca676b4afc38e8c
Konrad Laskowski <konrad374@gmail.com>
2022-01-30 20:52:00 +0100

    automated form working with selenium

--- seleniumTEST/main.py
+++ seleniumTEST/main.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import random
 
 option = webdriver.ChromeOptions()
-#option.add_argument("-incognito")
-option.add_argument("--headless")
+option.add_argument("-incognito")
+#option.add_argument("--headless")
 #option.add_argument("disable-gpu")
+
 
 mails =["konrad374@gmail.com","grazyna123@gmail.com",'zenek234@gmail.com',"jacek456@gmail.com"]
 names = ["jacek!", "paulina", "rafal", "alicja", "maciek"]
-b = 10
-browser = webdriver.Chrome(executable_path="chromedriver.exe",options=option)
+b = 1
+
+s=Service("chromedriver.exe")
+browser = webdriver.Chrome(service=s, options=option)
+
 while b > 0:
     b -= 1
 
@@ -19,14 +25,14 @@ while b > 0:
 
     browser.get("https://docs.google.com/forms/d/e/1FAIpQLSfg-un5ZEzQe7y_EOZVeTrVGa1sCX-MjYDkfPqNvOLqTRNl6Q/viewform")
 
-    textboxes=browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")
-    textboxes2=browser.find_elements_by_class_name("quantumWizTextinputPapertextareaInput")
+    textboxes=browser.find_element(By.CLASS_NAME, "quantumWizTextinputPaperinputPlaceholder")
+    #textboxes2=browser.find_element(By.CLASS_NAME, "quantumWizTextinputPapertextareaInput")
 
-    submit=browser.find_element_by_class_name("appsMaterialWizButtonPaperbuttonContent")
+    #submit=browser.find_element(By.CLASS_NAME, "appsMaterialWizButtonPaperbuttonContent")
 
-
+    print(textboxes)
     textboxes[0].send_keys(mails[a])
-    textboxes2[0].send_keys(names[c])
-    submit.click()
+    #textboxes2[0].send_keys(names[c])
+    #submit.click()
 
 
