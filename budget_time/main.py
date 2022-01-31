5a9ef93d628696dbd8bd6d29f48418dee2c96d4a
Konrad Laskowski <konrad374@gmail.com>
2022-02-01 00:12:54 +0100

    add shortcut class to whole program

--- budget_time/main.py
+++ budget_time/main.py
@@ -21,6 +21,109 @@ which_store = ""
 date = ""
 descr = ""
 
+#short_cut class - in future place in new module
+
+import ast
+
+class Shortcut:
+    def __init__(self,name):
+        self.dictionary = {}
+        self.file_name = f"sc_dict_{name}.txt"
+        self.name = name
+        try:
+            dict_file = open(self.file_name, 'rt')
+
+
+            if dict_file.readable():
+                file = dict_file.read()
+                if len(file) > 0:
+                    self.dictionary = ast.literal_eval(file)
+
+
+            try:
+                dict_file = open(self.file_name, 'wt')
+                dict_file.write(str(self.dictionary))
+                dict_file.close()
+
+            except:
+                print("Unable to write to file")
+        except:
+            # create fille if nescesery
+            dict_file = open(self.file_name, 'a+')
+            dict_file.write(str(self.dictionary))
+            print(f'* Utworzono plik ze slownikiem skrotow: {self.file_name}')
+
+    def add_shortcut_auto(self, short):
+        dict_file = open(self.file_name, 'rt')
+
+        if dict_file.readable():
+            file = dict_file.read()
+            if len(file) > 0:
+                self.dictionary = ast.literal_eval(file)
+        long = input(f'Podaj pelna nazwe dla  " {short} "  : ')
+        self.dictionary[short] = long
+
+        try:
+            dict_file = open(self.file_name, 'wt')
+            dict_file.write(str(self.dictionary))
+            dict_file.close()
+
+        except:
+            print("Unable to write to file")
+
+    def add_shortcut_manual(self):
+        dict_file = open(self.file_name, 'rt')
+
+        if dict_file.readable():
+            file = dict_file.read()
+            if len(file) > 0:
+                self.dictionary = ast.literal_eval(file)
+        print(f'-Dodaj skrot do slownika: {self.name}')
+        short = input("Skrot: ")
+        long = input(f'Podaj pelna nazwe dla  " {short} "  : ')
+        if short == "/back" or long == "/back":
+            ### go to adding bill function
+            pass
+        else:
+            self.dictionary[short] = long
+
+        try:
+            dict_file = open(self.file_name, 'wt')
+            dict_file.write(str(self.dictionary))
+            dict_file.close()
+
+        except:
+            print("Unable to write to file")
+
+    def use_shortcut(self, input0):
+        short = input0
+        try:
+            long = self.dictionary[short]
+            print(f'*** Uzyłeś skrótu do: {long}')
+            return long
+        except:
+            if len(short) < 4:
+                answer = input(f'***  " {short} "  ma mniej niz 4 znaki, czy chcesz dodać skrót? t/n: ')
+                if answer == 't' or answer == 'T':
+                    self.add_shortcut_auto(short)
+                    return self.dictionary[short]
+                else:
+                    return short
+            elif short == "/add":
+                print("***  Przeszedłes do funkcji dodawania skrotow do slownkika")
+                self.add_shortcut_manual()
+            else:
+                return short
+
+
+while True:
+    kategoria = Shortcut("kategorie")
+
+    print(kategoria.use_shortcut(input("podaj kategorie: ")))
+
+
+
+
 #apk_core
 class bill:
     file_name = "bill_file.txt"
